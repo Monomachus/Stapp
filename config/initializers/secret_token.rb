2ffc7a5b89e42fd16b0ab91ba1fff4c31a83d625
unknown <Monomachus@.(none)>
2010-12-14 21:53:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stapp::Application.config.secret_token = 'aaa3d3ebb850b28856e83d220b9b4678ebd4a3f3f520d6b5c0e889c5c6bc1683535b53c37df7ea7d5fdbf508530628b9411b405993dd9da92f0cd45845c7fad9'

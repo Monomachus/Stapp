c3e1c90cc500c3549a97aa95519024cdb8aa48fa
unknown <Monomachus@.(none)>
2010-12-15 00:52:00 +0200

    Added authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate, :except => [:index, :show]
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |login, password|
+      if user = User.find_by_login(login)
+        # Then compare the provided password against the hashed one in the db.
+        if user.password == password
+          return user
+        end
+      end
+
+      return nil
+    end
+  end
 end
--- app/models/product_operation.rb
+++ app/models/product_operation.rb
@@ -1,3 +1,4 @@
 class ProductOperation < ActiveRecord::Base
-  
+  validates :user_id, :product_id, :operation_type_id, :product_number,
+    :operation_date, :presence => true
 end
